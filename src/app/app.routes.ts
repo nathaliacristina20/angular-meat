81068dc846cca6b674b38baaf61a50c4f772d20f
Nathalia Oliveira <nathaliacristina20@hotmail.com>
2019-07-16 22:08:08 -0300

    criando formsmodule

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { RestaurantsComponent } from "./restaurants/restaurants.component";
 import { RestaurantDetailComponent } from "./restaurant-detail/restaurant-detail.component";
 import { MenuComponent } from "./restaurant-detail/menu/menu.component";
 import { ReviewsComponent } from "./restaurant-detail/reviews/reviews.component";
+import { OrderComponent } from "./order/order.component";
 
 export const ROUTES: Routes = [
     { path: '', component: HomeComponent},
@@ -15,6 +16,7 @@ export const ROUTES: Routes = [
             { path: '', redirectTo: 'menu', pathMatch: 'full'},
             { path: 'menu', component: MenuComponent},
             { path: 'reviews', component: ReviewsComponent}
-        ]}
+        ]},
+        { path: 'order', component: OrderComponent}
 ];
 
