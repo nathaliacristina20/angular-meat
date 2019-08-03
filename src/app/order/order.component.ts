8cd03ccac5595d1d7e0da900eed4ab240b6ed827
Nathalia Oliveira <nathaliacristina20@hotmail.com>
2019-08-02 21:06:47 -0300

    Validadores personalizados

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -4,7 +4,7 @@ import { OrderService } from './order.service';
 import { CartItem } from 'app/restaurant-detail/shopping-cart/cart-item.model';
 import { Order, OrderItem } from './order.model';
 import { Router } from '@angular/router';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'mt-order',
@@ -36,7 +36,19 @@ export class OrderComponent implements OnInit {
       number: this.formBuilder.control('', [Validators.required, Validators.pattern(this.numberPattern)]),
       optionalAddress: this.formBuilder.control(''),
       paymentOption: this.formBuilder.control('', [Validators.required])
-    });
+    }, { validator: OrderComponent.equalsTo });
+  }
+
+  static equalsTo(group: AbstractControl): {[key: string]: boolean} {
+    const email = group.get('email');
+    const emailConfirmation = group.get('emailConfirmation');
+    if (!email || !emailConfirmation){
+      return undefined;
+    }
+    if (email.value !== emailConfirmation.value){
+      return { emailsNoMatch: true }
+    }
+    return undefined;
   }
 
   itemsValue(): number {
